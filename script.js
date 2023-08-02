a0451d1623ab258563c3b393a9849804554e3155
shelly004 <nemashelly99@gmail.com>
2023-08-03 00:15:34 +0530

    js file changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let fieldinpt = document.getElementById('inpt');
 let result = document.getElementById("item");
 let taskArray1 = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
@@ -6,16 +5,15 @@ let taskArray1 = localStorage.getItem("items") ? JSON.parse(localStorage.getItem
 function createItem(data) {
     var ele = document.createElement("li");
     var text = document.createTextNode(data);
+    // text += `<span><button type="button">Delete</button></span>`
     ele.appendChild(text);
     result.appendChild(ele);
-}
 
+}
 
 taskArray1.forEach(item => {
     createItem(item);
 });
-
-
 
 function addValue(data) {
 
@@ -24,8 +22,8 @@ function addValue(data) {
     let taskArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
     taskArray.push(data);
     taskArray1 = taskArray
-
     localStorage.setItem("items", JSON.stringify(taskArray));
+    fieldinpt.innerHTML = " "
 
 }
 
@@ -36,3 +34,16 @@ function addItem() {
 
 }
 
+function sortList() {
+    let list = document.getElementById("item-list");
+    let Arrayitems = list.getElementsByTagName("li");
+    let sortedItems = Array.from(Arrayitems).sort((a, b) => a.innerText.localeCompare(b.innerText));
+
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+
+    for (let item of sortedItems) {
+        list.appendChild(item);
+    }
+}
